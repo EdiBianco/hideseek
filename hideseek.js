964e015516480c5b86374e97495f7334c5610a2a
EdiBianco <115667196+EdiBianco@users.noreply.github.com>
2024-11-13 18:26:02 -0300

    Update hideseek.js

--- hideseek.js
+++ hideseek.js
@@ -33,145 +33,194 @@
 
   */
 
-  (function($) {
-    "use strict";
+;(function($, window, undefined) {
+  "use strict";
 
-    $.fn.hideseek = function(options) {
-        var defaults = {
-            list: ".hideseek-data",
-            nodata: "",
-            attribute: "text",
-            matches: false,
-            highlight: false,
-            ignore: "",
-            headers: "",
-            navigation: false,
-            ignore_accents: false,
-            hidden_mode: false,
-            min_chars: 1
-        };
+  $.fn.hideseek = function(options) {
 
-        options = $.extend(defaults, options);
-
-        return this.each(function() {
-            var input = $(this);
-            input.opts = [];
-
-            $.map([
-                "list", "nodata", "attribute", "matches", "highlight",
-                "ignore", "headers", "navigation", "ignore_accents",
-                "hidden_mode", "min_chars"
-            ], function(option) {
-                input.opts[option] = input.data(option) || options[option];
-            });
-
-            if (input.opts.headers) {
-                input.opts.ignore += input.opts.ignore ? ", " + input.opts.headers : input.opts.headers;
-            }
-
-            var list = $(input.opts.list);
-
-            if (input.opts.navigation) {
-                input.attr("autocomplete", "off");
-            }
-
-            if (input.opts.hidden_mode) {
-                list.children().hide();
-            }
-
-            input.on('input', function(event) {
-                function showMatch(element) {
-                    if (input.opts.highlight) {
-                        element.removeHighlight().highlight(query).show();
-                    } else {
-                        element.show();
-                    }
-                }
-
-                function getSelected(element) {
-                    return element.children(".selected:visible");
-                }
-
-                function getPrev(element) {
-                    return getSelected(element).prevAll(":visible:first");
-                }
-
-                function getNext(element) {
-                    return getSelected(element).nextAll(":visible:first");
-                }
-
-                if ([38, 40, 13].indexOf(e.keyCode) === -1 && 
-    (e.keyCode !== 8 && e.keyCode !== 46 ? $this.val().length >= $this.opts.min_chars : true)) {
-                    var query = input.val().toLowerCase();
-
-                    list.children(input.opts.ignore.trim() ? ":not(" + input.opts.ignore + ")" : "")
-                        .removeClass("selected")
-                        .each(function() {
-                            var text = (input.opts.attribute !== "text" ? $(this).attr(input.opts.attribute) || "" : $(this).text()).toLowerCase();
-                            var notMatched = text.removeAccents(input.opts.ignore_accents).indexOf(query) === -1 || query === (input.opts.hidden_mode ? "" : false);
-
-                            if (notMatched) {
-                                $(this).hide();
-                            } else {
-                                showMatch($(this));
-
-                                if (input.opts.matches && query.match(new RegExp(Object.keys(input.opts.matches)[0]))) {
-                                    text.match(new RegExp(Object.values(input.opts.matches)[0])) ? showMatch($(this)) : $(this).hide();
-                                }
-                            }
-
-                            input.trigger("_after_each");
-                        });
-
-                    if (input.opts.nodata) {
-                        list.find(".no-results").remove();
-
-                        if (!list.children(':not([style*="display: none"])').length) {
-                            list.children().first().clone().removeHighlight().addClass("no-results").show().prependTo(input.opts.list).text(input.opts.nodata);
-                            input.trigger("_after_nodata");
-                        }
-                    }
-
-                    if (input.opts.headers) {
-                        $(input.opts.headers, list).each(function() {
-                            $(this).nextUntil(input.opts.headers).not('[style*="display: none"],' + input.opts.ignore).length ? $(this).show() : $(this).hide();
-                        });
-                    }
-
-                    input.trigger("_after");
-                }
-
-                if (input.opts.navigation) {
-                    if (event.keyCode === 38) {
-                        if (getSelected(list).length) {
-                            getPrev(list).addClass("selected");
-                            getSelected(list).last().removeClass("selected");
-                        } else {
-                            list.children(":visible").last().addClass("selected");
-                        }
-                    } else if (event.keyCode === 40) {
-                        if (getSelected(list).length) {
-                            getNext(list).addClass("selected");
-                            getSelected(list).first().removeClass("selected");
-                        } else {
-                            list.children(":visible").first().addClass("selected");
-                        }
-                    } else if (event.keyCode === 13) {
-                        if (getSelected(list).find("a").length) {
-                            document.location = getSelected(list).find("a").attr("href");
-                        } else {
-                            input.val(getSelected(list).text());
-                        }
-                    }
-                }
-            });
-        });
+    var defaults = {
+      list:           '.hideseek-data',
+      nodata:         '',
+      attribute:      'text',
+      matches:        false,
+      highlight:      false,
+      ignore:         '',
+      headers:        '',
+      navigation:     false,
+      ignore_accents: false,
+      hidden_mode:    false,
+      min_chars:      1
     };
 
-    $(document).ready(function() {
-        $('[data-toggle="hideseek"]').hideseek();
+    var options = $.extend(defaults, options);
+
+    return this.each(function() {
+
+      var $this = $(this);
+
+      $this.opts = [];
+
+      $.map( ['list', 'nodata', 'attribute', 'matches', 'highlight', 'ignore', 'headers', 'navigation', 'ignore_accents', 'hidden_mode', 'min_chars'], function( val, i ) {
+        $this.opts[val] = $this.data(val) || options[val];
+      } );
+
+      if ($this.opts.headers)
+        $this.opts.ignore += $this.opts.ignore ? ', ' + $this.opts.headers : $this.opts.headers;
+
+      var $list = $($this.opts.list);
+
+      if ($this.opts.navigation)  $this.attr('autocomplete', 'off');
+
+      if ($this.opts.hidden_mode) $list.children().hide();
+
+      $this.keyup(function(e) {
+
+        if ( [38, 40, 13].indexOf(e.keyCode) == -1 && ( e.keyCode != 8 && e.keyCode !== 46 ? $this.val().length >= $this.opts.min_chars : true ) ) {
+
+          var q = $this.val().toLowerCase();
+
+          $list.children($this.opts.ignore.trim() ? ":not(" + $this.opts.ignore + ")" : '').removeClass('selected').each(function() {
+
+            var data = (
+                        $this.opts.attribute != 'text'
+                          ? ($(this).attr($this.opts.attribute) || '')
+                          : $(this).text()
+                        ).toLowerCase();
+
+            var treaty = data.removeAccents($this.opts.ignore_accents).indexOf(q) == -1 || q === ($this.opts.hidden_mode ? '' : false)
+
+            if (treaty) {
+
+              $(this).hide();
+
+            } else {
+
+              show_element($(this));
+
+              if ($this.opts.matches && q.match(new RegExp(Object.keys($this.opts.matches)[0])) !== null) {
+                if (data.match(new RegExp(Object.values($this.opts.matches)[0])) !== null) {
+                  show_element($(this));
+                } else {
+                  $(this).hide();
+                }
+              }
+            }
+
+            $this.trigger('_after_each');
+
+          });
+
+          // No results message
+          if ($this.opts.nodata) {
+
+            $list.find('.no-results').remove();
+
+            if (!$list.children(':not([style*="display: none"])').length) {
+
+              $list
+                .children()
+                .first()
+                .clone()
+                .removeHighlight()
+                .addClass('no-results')
+                .show()
+                .prependTo($this.opts.list)
+                .text($this.opts.nodata);
+
+              $this.trigger('_after_nodata');
+
+            }
+
+          }
+
+          // hide headers with no results
+          if ($this.opts.headers) {
+            $($this.opts.headers, $list).each(function() {
+              if (!$(this).nextUntil($this.opts.headers).not('[style*="display: none"],' + $this.opts.ignore).length) {
+                $(this).hide();
+              } else {
+                $(this).show();
+              }
+            });
+          }
+
+          $this.trigger('_after');
+
+        };
+
+        function show_element(element) {
+          $this.opts.highlight ? element.removeHighlight().highlight(q).show() : element.show();
+        }
+
+        // Navigation
+        function current(element) {
+          return element.children('.selected:visible');
+        };
+
+        function prev(element) {
+          return current(element).prevAll(":visible:first");
+        };
+
+        function next(element) {
+          return current(element).nextAll(":visible:first");
+        };
+
+        if ($this.opts.navigation) {
+
+          if (e.keyCode == 38) {
+
+            if (current($list).length) {
+
+              prev($list).addClass('selected');
+
+              current($list).last().removeClass('selected');
+
+            } else {
+
+              $list.children(':visible').last().addClass('selected');
+
+            };
+
+          } else if (e.keyCode == 40) {
+
+            if (current($list).length) {
+
+              next($list).addClass('selected');
+
+              current($list).first().removeClass('selected');
+
+            } else {
+
+              $list.children(':visible').first().addClass('selected');
+
+            };
+
+          } else if (e.keyCode == 13) {
+
+            if (current($list).find('a').length) {
+
+              document.location = current($list).find('a').attr('href');
+
+            } else {
+
+              $this.val(current($list).text());
+
+            };
+
+          };
+
+        };
+
+      });
+
     });
-}(jQuery));
+
+  };
+
+  $(document).ready(function () { $('[data-toggle="hideseek"]').hideseek(); });
+
+})(jQuery);
+
 /*
 
 highlight v4
@@ -190,4 +239,14 @@ Johann Burkard
 jQuery.fn.highlight=function(t){function e(t,i){var n=0;if(3==t.nodeType){var a=t.data.removeAccents(true).toUpperCase().indexOf(i);if(a>=0){var s=document.createElement("mark");s.className="highlight";var r=t.splitText(a);r.splitText(i.length);var o=r.cloneNode(!0);s.appendChild(o),r.parentNode.replaceChild(s,r),n=1}}else if(1==t.nodeType&&t.childNodes&&!/(script|style)/i.test(t.tagName))for(var h=0;h<t.childNodes.length;++h)h+=e(t.childNodes[h],i);return n}return this.length&&t&&t.length?this.each(function(){e(this,t.toUpperCase())}):this},jQuery.fn.removeHighlight=function(){return this.find("mark.highlight").each(function(){with(this.parentNode.firstChild.nodeName,this.parentNode)replaceChild(this.firstChild,this),normalize()}).end()};
 
 // Ignore accents
-String.prototype.removeAccents=function(e){return e?this.replace(/[áàãâä]/gi,"a").replace(/[éè¨ê]/gi,"e").replace(/[íìïî]/gi,"i").replace(/[óòöôõ]/gi,"o").replace(/[úùüû]/gi,"u").replace(/[ç]/gi,"c").replace(/[ñ]/gi,"n"):this};
+String.prototype.removeAccents = function(enabled) {
+  if (enabled) return this
+                      .replace(/[áàãâä]/gi,"a")
+                      .replace(/[éè¨ê]/gi,"e")
+                      .replace(/[íìïî]/gi,"i")
+                      .replace(/[óòöôõ]/gi,"o")
+                      .replace(/[úùüû]/gi, "u")
+                      .replace(/[ç]/gi, "c")
+                      .replace(/[ñ]/gi, "n");
+  return this;
+};
